6a4307739af15912459412528a236630120af6fc
Juan Morales <juanmr2311@gmail.com>
2022-12-26 00:05:44 -0500

    Creación del proyecto con la configuración a la base de datos

---
+++ Bank-app-back/src/main/java/co/juan/bankappback/BankAppBackApplication.java
@@ -0,0 +1,13 @@
+package co.juan.bankappback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAppBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankAppBackApplication.class, args);
+    }
+
+}
---
+++ Bank-app-back/src/test/java/co/juan/bankappback/BankAppBackApplicationTests.java
@@ -0,0 +1,13 @@
+package co.juan.bankappback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAppBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
